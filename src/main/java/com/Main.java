0d717da68faf20c9a4d53855748e544d45e8b494
tranvancong <trancongtr1071997@gmail.com>
2020-06-27 19:01:09 +0700

    UDF for SQl syntax

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -9,32 +9,31 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
 import org.apache.spark.sql.types.DataTypes;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
         SparkSession spark = SparkSession.builder().appName("testingSql").master("local[*]").getOrCreate();
-        spark.udf().register("hasPassed",
-                (String grade, String subject) -> {
-                    if (subject.equals("Biology")) {
-                        if (grade.startsWith("A")) {
-                            return true;
-                        }
 
-                        return false;
-                    }
-                    
-                    return grade.startsWith("A") || grade.startsWith("B") || grade.startsWith("C");
+        Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/biglog.txt");
+        SimpleDateFormat input = new SimpleDateFormat("MMMM");
+        SimpleDateFormat output = new SimpleDateFormat("M");
+
+        spark.udf().register("monthNum",
+                (String month) -> {
+                    Date inputDate = input.parse(month);
+                    return Integer.parseInt(output.format(inputDate));
                 },
-                DataTypes.BooleanType
-        );
+                DataTypes.IntegerType);
 
-        Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
+        dataset.createOrReplaceTempView("logging_table");
 
-        dataset = dataset.withColumn("pass",
-                functions.callUDF("hasPassed", functions.col("grade"), functions.col("subject"))
-        );
+        Dataset<Row> results = spark.sql("select level, date_format(datetime, 'MMMM') as month, count(1) as total " +
+                "from logging_table group by level, date_format(datetime, 'MMMM') order by monthNum(month), level");
 
-        dataset.show();
+        results.show();
     }
 }
