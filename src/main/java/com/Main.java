a57ba576b92bc78344270418bd13fa312ccc83de
tranvancong <trancongtr1071997@gmail.com>
2020-06-27 12:38:45 +0700

    in memory data

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,20 +2,33 @@ package com;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
         SparkSession spark = SparkSession.builder().appName("testingSql").master("local[*]").getOrCreate();
-        Dataset<Row> dataset = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
+        List<Row> inMemory = new ArrayList<>();
+        inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
+        inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
+        inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
+        inMemory.add(RowFactory.create("INFO", "2016-4-11 14:32:21"));
+        inMemory.add(RowFactory.create("FATAL", "2016-4-21 19:23:20"));
 
-        dataset.createOrReplaceTempView("my_students_table");
-        Dataset<Row> results = spark.sql("select distinct(year) from my_students_table where subject='French' order by year");
-        results.show();
+        StructField[] fields = new StructField[]{
+                new StructField("level", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("datetime", DataTypes.StringType, false, Metadata.empty())
+        };
+        StructType schema = new StructType(fields);
+        Dataset<Row> dataset = spark.createDataFrame(inMemory, schema);
+        dataset.show();
     }
 }
